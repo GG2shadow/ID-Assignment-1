b355d18372a7508c8aef71418d6c016209d6a497
GG2shadow <xyj.mine@gmail.com>
2021-11-28 13:56:31 +0800

    Added favicon and on index.html: Hero-banner, Finals Info, Meet the Teams, Get Ready for EDG vs DK.

---
+++ script.js
@@ -0,0 +1,5 @@
+/* Transparent Header turns White Background with Black Text on Scroll */
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
